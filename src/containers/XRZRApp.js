0c50b127381f5c80f209b14726ed2ddd6bd6837c
Aakash Sigdel <aakash.sigdel@gmail.com>
2016-02-04 17:06:36 +0545

    add boilerplate to link components file

---
+++ src/containers/XRZRApp.js
@@ -0,0 +1,13 @@
+'use strict'
+
+import React from 'react-native'
+
+import Login from '../components/Login/Login'
+
+const XRZRApp = props => {
+  return (
+    <Login />
+  )
+}
+
+export default XRZRApp
