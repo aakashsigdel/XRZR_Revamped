e4df03d7e5632db53b42a407d962ef5c2644eced
Aakash Sigdel <aakash.sigdel@gmail.com>
2016-02-19 13:46:50 +0545

    add instructor name

--- src/redux_x/reducers/instructor.js
+++ src/redux_x/reducers/instructor.js
@@ -3,17 +3,22 @@
 const initialState = {
   0: {
     id: 0,
-    name: 'Santi Cazorla',
+    name: 'Kalle Fit',
     image: 'http://www.arsenalsite.cz/imgs/soupiska/200/santi-cazorla.jpg'
   },
   1: {
     id: 1,
-    name: 'Hector Bellerin',
+    name: 'Maria Barfod',
     image: 'http://2.bp.blogspot.com/-2jejibP1zOM/Vcs0FTid1iI/AAAAAAAAAoE/M78ZFS9NfIo/s200-c/217696.jpg'
   },
   2: {
     id: 2,
-    name: 'Theo Walcott',
+    name: 'Stina Troest',
+    image: 'http://www.ulasbola.com/wp-content/uploads/2013/01/Theo-Walcott1.jpg'
+  },
+  3: {
+    id: 3,
+    name: 'Rachel Grey',
     image: 'http://www.ulasbola.com/wp-content/uploads/2013/01/Theo-Walcott1.jpg'
   }
 }
