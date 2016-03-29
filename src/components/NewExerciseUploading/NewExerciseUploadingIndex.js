90ecc780433bd6fcf13e7fce13d9198bcd7f8fbd
Sushil <afahocci@gmail.com>
2016-03-29 16:44:28 +0545

    upload state moved to controller

--- src/components/NewExerciseUploading/NewExerciseUploadingIndex.js
+++ src/components/NewExerciseUploading/NewExerciseUploadingIndex.js
@@ -1,6 +1,5 @@
 import React, {
   Component,
-  LayoutAnimation,
   Modal,
   StyleSheet,
   View
@@ -9,58 +8,57 @@ import Navigation from './Navigation'
 import Header from './Header'
 import Info from './Info'
 import ProgressBar from './ProgressBar'
-import {sendAjax} from '../../utilities/SendAjax'
 
 export default class NewExerciseUploadingIndex extends Component {
   constructor(props) {
     super(props)
 
-    this.state = {
-      completed: 0
-    }
+    //this.state = {
+    //  completed: 0
+    //}
   }
 
-  componentDidMount () {
-    console.warn('start')
-    let a = new FormData()
-    a.append('title', this.props.exercise.title)
-    a.append('description', this.props.exercise.description)
-    a.append('tags', this.props.exercise.tags)
-    a.append('sound', this.props.exercise.sound)
-
-    console.log(this.props.exercise)
-    a.append('video', {uri: this.props.exercise.videoUri, name: 'hello.mov', type: 'video/mov'})
-
-    sendAjax({
-      type: 'post',
-      url: 'https://xrzr.backlect.com/api/xrzr/v1.0/exercise',
-      beforeSend: function (xhr) {
-        xhr.setRequestHeader('Accept', 'application/json, text/javascript')
-      },
-      success: function (res) {
-        console.log(res)
-      },
-      error: function (a) {
-        console.warn('error')
-        console.log(a)
-      },
-      complete: function (w) {
-        console.warn('complete')
-        console.log(w)
-      },
-      progress: function (evt) {
-        console.log('hello')
-        if (evt.lengthComputable) {
-          var percentComplete = (evt.loaded / evt.total) * 100
-          LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut)
-          this.setState({completed: percentComplete})
-        }
-      }.bind(this),
-      formData: a,
-      cache: true
-
-    })
-  }
+  //componentDidMount () {
+  //  console.warn('start')
+  //  let a = new FormData()
+  //  a.append('title', this.props.exercise.title)
+  //  a.append('description', this.props.exercise.description)
+  //  a.append('tags', this.props.exercise.tags)
+  //  a.append('sound', this.props.exercise.sound)
+  //
+  //  console.log(this.props.exercise)
+  //  a.append('video', {uri: this.props.exercise.videoUri, name: 'hello.mov', type: 'video/mov'})
+  //
+  //  sendAjax({
+  //    type: 'post',
+  //    url: 'https://xrzr.backlect.com/api/xrzr/v1.0/exercise',
+  //    beforeSend: function (xhr) {
+  //      xhr.setRequestHeader('Accept', 'application/json, text/javascript')
+  //    },
+  //    success: function (res) {
+  //      console.log(res)
+  //    },
+  //    error: function (a) {
+  //      console.warn('error')
+  //      console.log(a)
+  //    },
+  //    complete: function (w) {
+  //      console.warn('complete')
+  //      console.log(w)
+  //    },
+  //    progress: function (evt) {
+  //      console.log('hello')
+  //      if (evt.lengthComputable) {
+  //        var percentComplete = (evt.loaded / evt.total) * 100
+  //        LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut)
+  //        this.setState({completed: percentComplete})
+  //      }
+  //    }.bind(this),
+  //    formData: a,
+  //    cache: true
+  //
+  //  })
+  //}
 
   render (props = this.props) {
     return (
@@ -79,8 +77,8 @@ export default class NewExerciseUploadingIndex extends Component {
         />
         <Info />
         <ProgressBar
-          completed={this.state.completed}
-          remaining={100 - this.state.completed}
+          completed={this.props.completed}
+          remaining={100 - this.props.completed}
         />
       </View>
       </Modal>
