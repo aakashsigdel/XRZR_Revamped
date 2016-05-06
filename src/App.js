78c701780088fb2ea1432c677ee7e8ebcb654a6d
Sushil <afahocci@gmail.com>
2016-05-06 16:01:19 +0545

    category presist the state

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ require('es6-promise').polyfill()
 import React from 'react-native'
 import { Provider } from 'react-redux'
 import { createStore, compose, applyMiddleware } from 'redux'
+import {persistStore, autoRehydrate} from 'redux-persist'
 import thunkMiddleware from 'redux-thunk'
 
 import devTools from 'remote-redux-devtools'
@@ -18,8 +19,11 @@ const store = createStore(
       thunkMiddleware
     ),
     devTools({name: 'aakash'})
-  )
+  ),
+  autoRehydrate()
 )
+
+persistStore(store, {whitelist: 'category', storage: React.AsyncStorage})
 
 const App = () => {
   return (
